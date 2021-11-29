801431973b00e82e31344f4e3082c4c89f9daf56
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-11-29 13:42:20 -0500

    Update imagenet-r-eval.py

--- imagenet-r-eval.py
+++ imagenet-r-eval.py
@@ -139,7 +139,7 @@ def validate(val_loader, model, criterion, args):
 
         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
               .format(top1=top1, top5=top5))
-
+    np.save('/content/gdrive/MyDrive/model_OOD_acc/imagenet-r/simclr.npy',top1.avg)
     return top1.avg
 
 
