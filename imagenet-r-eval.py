d22cf5daee5ae8409c70aa1ae6320ea2d0ae0536
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-11-29 09:27:22 -0500

    Update imagenet-r-eval.py

--- imagenet-r-eval.py
+++ imagenet-r-eval.py
@@ -51,7 +51,7 @@ std = [0.229, 0.224, 0.225]
 test_transform = transforms.Compose(
     [transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean, std)])
 
-naes = datasets.ImageFolder(root="/content/gdrive/MyDrive/imagenet-a", transform=test_transform)
+naes = datasets.ImageFolder(root="/content/gdrive/MyDrive/imagenet-r", transform=test_transform)
 nae_loader = torch.utils.data.DataLoader(naes, batch_size=64, shuffle=False,
                                          num_workers=4, pin_memory=True)
 
